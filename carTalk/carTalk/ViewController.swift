0639ef70dee9bb4cdc8d3e80e8188f29f109f831
YongSangHo <yonghole@ajou.ac.kr>
2021-02-13 15:40:45 +0900

    project created

---
+++ carTalk/carTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  carTalk
+//
+//  Created by 용상호 on 2021/02/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
