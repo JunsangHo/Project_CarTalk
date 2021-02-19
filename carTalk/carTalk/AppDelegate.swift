fcb740b4f077f4487cc2ad1b8caa491baa6a0f27
YongSangHo <yonghole@ajou.ac.kr>
2021-02-19 16:30:58 +0900

    fixed auto layout

--- carTalk/carTalk/AppDelegate.swift
+++ carTalk/carTalk/AppDelegate.swift
@@ -12,13 +12,13 @@ import Firebase
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
