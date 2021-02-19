64dd785f68c0645b36c6cd4380e9d597813557d2
YongSangHo <yonghole@ajou.ac.kr>
2021-02-19 15:51:50 +0900

    fix selection

--- carTalk/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/leveldb-library.build/Objects-normal/x86_64/filter_policy.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/yongsangho/Desktop/Dev/carTalkProj/Project_CarTalk/carTalk/Pods/leveldb-library/util/filter_policy.cc \
-  /Users/yongsangho/Desktop/Dev/carTalkProj/Project_CarTalk/carTalk/Pods/Target\ Support\ Files/leveldb-library/leveldb-library-prefix.pch \
-  /Users/yongsangho/Desktop/Dev/carTalkProj/Project_CarTalk/carTalk/Pods/leveldb-library/include/leveldb/filter_policy.h \
-  /Users/yongsangho/Desktop/Dev/carTalkProj/Project_CarTalk/carTalk/Pods/leveldb-library/include/leveldb/export.h
