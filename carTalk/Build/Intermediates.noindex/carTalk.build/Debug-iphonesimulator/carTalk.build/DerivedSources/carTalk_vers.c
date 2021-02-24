475b6e8b89f9d5cbe1712ae91a6ded256964a551
YongSangHo <yonghole@ajou.ac.kr>
2021-02-24 11:01:37 +0900

    database updated

---
+++ carTalk/Build/Intermediates.noindex/carTalk.build/Debug-iphonesimulator/carTalk.build/DerivedSources/carTalk_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char carTalkVersionString[];
+ extern const double carTalkVersionNumber;
+
+ const unsigned char carTalkVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:carTalk  PROJECT:carTalk-3" "\n";
+ const double carTalkVersionNumber __attribute__ ((used)) = (double)3.;
